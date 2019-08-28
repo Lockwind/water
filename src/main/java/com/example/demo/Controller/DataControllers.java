959aab1470370594fca6749149d7f015d88ef3c8
userhan <454170399@qq.com>
2019-08-28 03:42:31 -0400

    new update

--- src/main/java/com/example/demo/Controller/DataControllers.java
+++ src/main/java/com/example/demo/Controller/DataControllers.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.crypto.Data;
-
 @RestController
 public class DataControllers {
     @Autowired
